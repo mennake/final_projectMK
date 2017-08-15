691c4171b804514e54c0e1f275ecd69c099fa516
Menna Kearns <mennake@gmail.com>
2017-08-14 21:06:45 -0700

    geolocation code

--- js/main.js
+++ js/main.js
@@ -14,8 +14,36 @@ var config = {
     
 $(document).ready(function() {
 
+  $(document).on('click','.findAllTheTacos',function(e){
+  //function findAllTheTacos() {
+    var output = document.getElementById("taco-trucks");
 
+    if (!navigator.geolocation){
+      output.innerHTML = "<p>Geolocation not supported</p>";
+      return;
+    }
 
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
 
-})
+      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+      var img = new Image();
+      img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+      output.appendChild(img);
+    }
+
+    function error() {
+      output.innerHTML = "Unable to get your location :-(";
+    }
+
+    output.innerHTML = "<div id=\"popUp\" class=\"loader\"><p>Finding you deliciousness…</p></div>";
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  //
+  });
+
+});
 
