78853af5e7f5b59f8496beab4cf3d25c1a696c3a
Menna Kearns <mennake@gmail.com>
2017-08-14 20:22:46 -0700

    added firebase info

---
+++ js/main.js
@@ -0,0 +1,21 @@
+    // Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyDhgbnhm3x8CvStik5r9rN-2Jqk2Nw3H_I",
+    authDomain: "final-project-75da0.firebaseapp.com",
+    databaseURL: "https://final-project-75da0.firebaseio.com",
+    projectId: "final-project-75da0",
+    storageBucket: "final-project-75da0.appspot.com",
+    messagingSenderId: "985300721437"
+  };
+  firebase.initializeApp(config);
+  
+  var messageAppReference = firebase.database();
+    
+$(document).ready(function() {
+
+
+
+
+})
+
